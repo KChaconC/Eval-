7cb07fa17a6a7fd3392ed92f602f182ac4f3bca6
Kathe <36324710+KChaconC@users.noreply.github.com>
2018-04-24 03:11:33 -0600

    codigo-semilla

---
+++ public/app.js
@@ -0,0 +1,8 @@
+(() => {
+    'use strict';
+    angular
+      .module('evalu', [
+        'appRoutes', 
+        'ngMap'
+      ]);
+  })();
---
+++ public/clases.js
@@ -0,0 +1,7 @@
+class Usuario {
+    constructor()
+}
+
+class Hotel {
+    constructor()
+}
